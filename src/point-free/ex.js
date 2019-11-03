2c7a19bcf732ffb733f09700ad70fd3eae85805e
Tom Mulkins <tommulkins@Toms-Air.lan1>
2019-11-03 16:54:23 -0500

    point free exercises

--- src/point-free/ex.js
+++ src/point-free/ex.js
@@ -1,29 +1,33 @@
-"use strict";
+'use strict'
 
-function output(txt) {
-	console.log(txt);
+function not(predicate) {
+  return function negated(...args) {
+    return !predicate(...args)
+  }
 }
 
-function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
-	};
+function when(fn) {
+  return function(predicate) {
+    return function(...args) {
+      if (predicate(...args)) {
+        return fn(...args)
+      }
+    }
+  }
 }
+
+const output = console.log
+const printIf = when(output)
+const isLongEnough = not(isShortEnough)
 
 function isShortEnough(str) {
-	return str.length <= 5;
+  return str.length <= 5
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+var msg1 = 'Hello'
+var msg2 = msg1 + ' World'
 
-var msg1 = "Hello";
-var msg2 = msg1 + " World";
-
-printIf(isShortEnough)(msg1);		// Hello
-printIf(isShortEnough)(msg2);
-printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isShortEnough)(msg1) // Hello
+printIf(isShortEnough)(msg2)
+printIf(isLongEnough)(msg1)
+printIf(isLongEnough)(msg2) // Hello World
